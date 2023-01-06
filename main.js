c7c0ca9445cdc502fa01834baf327b180b85110c
Rodrigo Santos <rssantos07@gmail.com>
2023-01-06 11:59:22 -0300

    exercicio validação formulario com javascript

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('formulario');
+vsco
+form.addEventListener('submit',function (e){
+    
+    e.preventDefault();
+    
+    let valorA = parseInt(document.getElementById('valor-a').value);
+    let valorB = parseInt(document.getElementById('valor-b').value);
+    const valido = `Esse Formulário <b>É</b> Válido`
+    const naoValido = `Esse formulário <b>Não</b> é Válido`
+
+    
+
+    if (valorB>valorA){
+        const containerMensagem =document.querySelector('.message');
+        containerMensagem.innerHTML = valido;
+        document.querySelector('.message').style.display = 'block';
+        document.querySelector('.error-message').style.display= 'none'
+
+    }else{
+        const containerMensagem =document.querySelector('.error-message');
+        containerMensagem.innerHTML = naoValido;
+        document.querySelector('.error-message').style.display= 'block'
+        document.querySelector('.message').style.display = 'none';
+        
+    }
+})
+
