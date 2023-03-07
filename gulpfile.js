af85eac42bc2338c7217737a5f4481d179312669
Rodrigo Santos <rssantos07@gmail.com>
2023-03-07 00:16:49 -0300

    gulpfile exercise

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');  
+const imagemin = require('gulp-imagemin');
+const funcao = require('./source/scripts/funcao');
+
+
+gulp.task('funcao', function() {
+    return funcao();
+  });
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'))
+}
+
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+
+
+function compilaSass() {
+    return gulp.src('./source/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build'));
+}
+
+
+exports.default = function () {
+    gulp.watch('./source/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
+    gulp.watch('./source/scripts/funcao', { ignoreInitial: false }, gulp.series(funcao));
+}
